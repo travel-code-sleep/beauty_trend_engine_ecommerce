376d2a53cf460d2d71bbc2bf400665837aacecf3
SRV MYMTEP UAT ALP <SRVMYMTEPUATALP@ALPHA.lftltd.net>
2019-10-22 05:27:16 +0000

    file_delete_options_Added

--- meiyume_pkg/meiyume/sph_crawler.py
+++ meiyume_pkg/meiyume/sph_crawler.py
@@ -8,7 +8,7 @@ from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-from utils import Logger, Browser
+from .utils import Logger, Browser
 import shutil
 
 
