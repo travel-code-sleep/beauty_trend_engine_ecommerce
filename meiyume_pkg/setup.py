c6638dcccf297f8d98daf9d86e12f32e23c24e88
SRV MYMTEP UAT ALP <SRVMYMTEPUATALP@ALPHA.lftltd.net>
2019-10-23 06:27:36 +0000

    readme_update

--- meiyume_pkg/setup.py
+++ meiyume_pkg/setup.py
@@ -13,7 +13,7 @@ setup(
                       'missingno', 'matplotlib', 'tldextract',
                       'tqdm', 'plotly', 'seaborn', 'sklearn'],
     # *strongly* suggested for sharing
-    version='0.5.2',
+    version='0.5.3',
     # The license can be anything you like
     license='MIT',
     description='Contains all codes for data scraping and cleaning.',
