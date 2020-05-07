96fee45d8914301d2c740284870fa40451562bf7
SRV MYMTEP UAT ALP <SRVMYMTEPUATALP@ALPHA.lftltd.net>
2020-05-07 16:09:23 +0000

    resolve envirenotments and setup

---
+++ meiyume_pkg/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='meiyume',
+    url='https://github.com/travel-code-sleep/meiyume_master_source_codes',
+    author='Amit Prusty',
+    author_email='dcsamit@nus.edu.sg',
+    # Needed to actually package something
+    packages=find_packages(),
+    namespace_packages=['meiyume'],
+    # Needed for dependencies
+    # install_requires=['numpy', 'logging', 'pandas', 'selenium', 'missingno', 'spacy',
+    #                   'fastai', 'torch', 'torchvision', 'unidecode', 'boto3',
+    #                   'matplotlib', 'tldextract', 'tqdm', 'plotly', 'seaborn',
+    #                   'sklearn', 'swifter', 'keras', 'tensorflow', 'scipy', 'pyarrow',
+    #                   ],
+    # *strongly* suggested for in-house use
+    version='0.6.1',
+    # The license can be anything you like
+    license='Private',
+    description='Contains all codes for data scraping and cleaning.',
+    # We will also need a readme eventually (there will be a warning)
+    long_description=open('README.md').read(),
+)
